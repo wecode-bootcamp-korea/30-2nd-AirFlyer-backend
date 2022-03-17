c7dfa200e1740ee115e13fb70bd60d23ae3d9267
ahn <sungjoonan02@gmail.com>
2022-03-17 17:36:11 +0900

    항공편/해성 조회 view 기능 구현

--- flights/tests.py
+++ flights/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+from flights.models import Planet, Flight
+class PlanetListViewTest(TestCase):
+    def setUp(self):
+        client = Client()
+
+        Planet.objects.create(
+            id   = 1,
+            name = "지구",
+            code = "ERH"
+        )
+
+        Planet.objects.create(
+            id   = 2,
+            name = "달",
+            code = "MON"
+        )
+
+        Planet.objects.create(
+            id   = 3,
+            name = "수성",
+            code = "MCR"
+        )
+
+    def tearDown(self) :
+        Planet.objects.all().delete()
+        
+    def test_success_view_get_planet_list(self):
+        client = Client()
+        response = client.get('/flights/planet')
+
+        self.assertEqual(response.json(),{
+            "planet_list": [
+                {
+                    "planet_id" : 1,
+                    "planet_name" : "지구",
+                    "planet_code" : "ERH"
+                }, 
+                {
+                    "planet_id": 2,
+                    "planet_name": "달",
+                    "planet_code": "MON"
+                },
+                {
+                    "planet_id": 3,
+                    "planet_name": "수성",
+                    "planet_code": "MCR"
+                }
+            ]
+        })
+        self.assertEqual(response.status_code, 200)
+
+
+class FlightListViewTest(TestCase):
+    def setUp(self):
+        client = Client()
+
+        Planet.objects.create(
+            id   = 1,
+            name = "지구",
+            code = "ERH"
+        )
+
+        Planet.objects.create(
+            id   = 2,
+            name = "달",
+            code = "MON"
+        )
+
+        Flight.objects.create(
+            id = 1,
+            name = "보급형우주선"
+        )
+
+        
+
+
+
--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+import requests, json, datetime
 
-# Create your views here.
+from datetime import date, datetime, timedelta
+from posixpath import split
+
+from django.http      import JsonResponse
+from django.views     import View
+from django.db.models import Q, Count
+
+from flights.models import Flight, FlightSchedule, Planet, Seat, FlightSeat
+
+class PlanetListView(View):
+    def get(self, request):
+        
+        planets = Planet.objects.all()
+
+        result = [
+            {
+                'planet_id'   : planet.id,
+                'planet_name' : planet.name,
+                'planet_code' : planet.code
+            } for planet in planets]
+
+        return JsonResponse({'planet_list' : result}, status = 200)
+
+class FlightListView(View):
+    def get(self, request):
+        sort                = request.GET.get('sort', 'departure_time')
+        departure_planet_id = request.GET.get('departure_planet_id', None)
+        arrival_planet_id   = request.GET.get('arrival_planet_id', None)
+        departure_date      = request.GET.get('departure_date', None)
+
+        sort_set = {
+            "departure_time" : "departure_datetime", 
+            "arrival_time"   : "arrival_datetime",
+            "duration"       : "duration",
+            "price"          : "default_price"
+        }
+        
+        q = Q()
+
+        if departure_planet_id and arrival_planet_id:
+            q &= Q(departure_planet_id = departure_planet_id)
+            q &= Q(arrival_planet_id   = arrival_planet_id)
+        
+        if departure_date:
+            departure_datetime = datetime.strptime(departure_date, "%Y-%m-%d")
+            q &= Q(departure_datetime__gte = departure_datetime)
+            q &= Q(departure_datetime__lt  = departure_datetime + timedelta(days=1))
+
+
+        flight_schedules = FlightSchedule.objects.filter(q).order_by(sort_set.get(sort, "departure_datetime"))
+
+        result = [{
+            'flight_schedule_info' : {
+                'flight_schedule_id'    : flight_schedule.id,
+                'departure_planet_code' : flight_schedule.departure_planet.code,
+                'arrival_planet_code'   : flight_schedule.arrival_planet.code,
+                'departure_datetime'    : flight_schedule.departure_datetime,
+                'arrival_datetime'      : flight_schedule.arrival_datetime,
+                'duration'              : str(flight_schedule.duration) + ":00", 
+                'spaceship_name'        : flight_schedule.flight.name
+            },
+            'flight_seat_info' : [{
+                'seat_id'     : flightseat.seat.id,
+                'seat_type'   : flightseat.seat.type,
+                'seat_remain' : flightseat.seat_availability - \
+                                flight_schedule.passengerinformation_set.filter(seat=flightseat.seat).count(),
+                'seat_price'  : int(flight_schedule.default_price * flightseat.seat.price_ratio/100)
+            }for flightseat in flight_schedule.flight.flightseat_set.all()]
+        }for flight_schedule in flight_schedules]
+
+        return JsonResponse({'result' : result}, status = 200)
