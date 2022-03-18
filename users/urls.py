29fc23507a98304a52171196bba63d01861cc357
김산 <cloudhat17@gmail.com>
2022-03-18 18:01:22 +0900

    해시맵만 쓰기로 변경 및 필요없는 import 제거

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views      import SocialSignIn
+from .views import SocialSignIn,ReservationView
 
 urlpatterns = [
     path('/signin',SocialSignIn.as_view()),
+    path('/reservation',ReservationView.as_view()),
 ]
