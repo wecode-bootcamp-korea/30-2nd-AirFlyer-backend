982936688e168aa95a1790db0848c1e1bfe06b95
김산 <cloudhat17@gmail.com>
2022-03-23 15:31:01 +0900

    클래스명 변경

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SocialSignIn,ReservationView
+from .views import SocialSignIn,ReservationView,ReservationHistoryView
 
 urlpatterns = [
     path('/signin',SocialSignIn.as_view()),
     path('/reservation',ReservationView.as_view()),
+    path('/mypage',ReservationHistoryView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.http      import JsonResponse
 from django.views     import View
 from django.db.models import F
 from django.db import transaction
+from django.db.models import Sum
 
 from users.models     import User ,Reservation, PassengerInformation
 from .decorator       import login_decorator
@@ -38,7 +39,7 @@ class SocialSignIn(View):
 
                 status_code      = 201 if is_created else 200
                 token            = jwt.encode({"user_id":user.id}, SECRET_KEY, ALGORITHM)
-                
+
                 return JsonResponse({"token": token},status = status_code)
                 
             
@@ -52,7 +53,7 @@ class ReservationView(View):
     def post(self,request):
         try:
             data =  json.loads(request.body.decode('utf-8'))
-
+            
             user         = request.user
             total_price  = data['total_price']
             user.mileage = F('mileage')-total_price
@@ -62,27 +63,72 @@ class ReservationView(View):
                 reservation = Reservation.objects.create(user_id=user.id)
 
             passenger_informations = data['passenger_informations']
-            
-            number_of_passenger    = len(passenger_informations)
             seat_hashmap           = {'이코노미':'1',"비즈니스":'2'} 
             
             for passenger_information in passenger_informations:   
+                
                 with transaction.atomic():
                     PassengerInformation.objects.create(
-                                first_name         = passenger_information['first_name'],
-                                last_name          = passenger_information['last_name'],
-                                gender             = passenger_information['gender'],
-                                email              = passenger_information['email'],
-                                birth_date         = passenger_information['birth_date'],
-                                price              = total_price/number_of_passenger ,
-                                reservation_id     = reservation.id,
-                                flight_schedule_id = passenger_information['flight_schedule_id'],
-                                seat_id            = seat_hashmap[passenger_information['seat_name']]
-                                )
+                            first_name         = passenger_information['first_name'],
+                            last_name          = passenger_information['last_name'],
+                            gender             = passenger_information['gender'],
+                            email              = passenger_information['email'],
+                            birth_date         = passenger_information['birth_date'],
+                            price              = total_price/len(passenger_informations) ,
+                            reservation_id     = reservation.id,
+                            flight_schedule_id = passenger_information['flight_schedule_id'],
+                            seat_id            = seat_hashmap[passenger_information['seat_name']]
+                            )
                     
-
             return JsonResponse({"message":'success'},status=201)
 
         except KeyError:
             return JsonResponse({"message" : "KEYERROR"}, status = 400)
-        
+
+class ReservationHistoryView(View):
+    
+    @login_decorator 
+    def get(self,request):
+        try:
+            user = request.user
+            result = []
+            user_reservations = Reservation.objects.filter(user_id=user.id).prefetch_related(
+                'passengerinformation_set__seat',
+                'passengerinformation_set__flight_schedule__arrival_planet',
+                'passengerinformation_set__flight_schedule__departure_planet'
+                )
+
+            for reservation in user_reservations:
+                passenger_informations = reservation.passengerinformation_set.filter(reservation_id=reservation.id)
+                
+                reservation_information = {}
+
+                reservation_information['reservation_id'] = reservation.id
+                
+                reservation_information['seat'] = {
+                    'seat_type'   : passenger_informations[0].seat.type,
+                    'seat_number' : len(passenger_informations)
+                }
+                
+                reservation_information['total_price'] = int(passenger_informations.aggregate(Sum("price"))['price__sum'])
+                
+                departure_datetime = str(passenger_informations[0].flight_schedule.departure_datetime).split(':')
+                arrival_datetime   = str(passenger_informations[0].flight_schedule.arrival_datetime).split(':')
+                
+                reservation_information['flight_schedule'] = {
+                    'departure_datetime'    : departure_datetime[0]+':'+departure_datetime[1],
+                    'arrival_datetime'      : arrival_datetime[0]+':'+arrival_datetime[1],
+                    'duration'              : passenger_informations[0].flight_schedule.duration,
+                    'departure_planet_name' : passenger_informations[0].flight_schedule.departure_planet.name,
+                    'departure_planet_code' : passenger_informations[0].flight_schedule.departure_planet.code,
+                    'arrival_planet_name'   : passenger_informations[0].flight_schedule.arrival_planet.name,
+                    'arrival_planet_code'   : passenger_informations[0].flight_schedule.arrival_planet.code,
+                }
+                
+                result.append(reservation_information)    
+                
+                    
+            return JsonResponse({"data":result},status=200)
+
+        except KeyError:
+            return JsonResponse({"message" : "KEYERROR"}, status = 400)              
