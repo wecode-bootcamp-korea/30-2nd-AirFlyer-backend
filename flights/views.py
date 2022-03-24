95634c3f105c53d88908fae7d920f2fa5309d48f
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2022-03-24 11:15:37 +0900

    mentor review

--- flights/views.py
+++ flights/views.py
@@ -26,66 +26,69 @@ class PlanetListView(View):
 
 class FlightListView(View):
     def get(self, request):
-        flight_schedule_id  = request.GET.get('flight_schedule_id', None)
+
+        """
+        목적: 플라이트 스케줄 리스트 정보 get
+        조건:
+            1. 출발지, 도착지
+            2. 입력한 날짜에 예약이 가능한 스케줄
+        정렬:
+            1. 출발 시간
+            2. 도착 시간
+            3. 비행 시간
+            4. 가격 
+
+        
+        """
+        sort                = request.GET.get('sort', 'departure_time')
         departure_planet_id = request.GET.get('departure_planet_id', None)
         arrival_planet_id   = request.GET.get('arrival_planet_id', None)
         departure_datetime  = request.GET.get('departure_datetime', None)
-        arrival_datetime    = request.GET.get('arrival_datetime', None)
-
-        departure_datetime        = departure_datetime.split(" ") 
-        departure_datetime_string = departure_datetime[0]+' '+departure_datetime[1]
-        departure_datetime        = datetime.strptime(departure_datetime_string, '%Y-%m-%d %H:%M:%S')
         
-        arrival_datetime        = arrival_datetime.split(" ")
-        arrival_datetime_string = arrival_datetime[0]+' '+arrival_datetime[1]
-        arrival_datetime        = datetime.strptime(arrival_datetime_string, '%Y-%m-%d %H:%M:%S')
         
-        economy_seat_price  = int(FlightSchedule.objects.get(id=flight_schedule_id).default_price)
-        business_seat_price = int(FlightSchedule.objects.get(id=flight_schedule_id).default_price) * Seat.objects.get(type = '비즈니스').price_ratio/100
-
-        # eco = flight_schedule.flight.seats.all()[0]
-        # eco.type ==> '이코노미'
-        # eco_seat = flight_schedule.flight.flightseat_set.get(seat=eco).seat_availability
+        # departure_datetime        = departure_datetime.split(" ") 
+        # departure_datetime_string = departure_datetime[0]+' '+departure_datetime[1]
+        # departure_datetime        = datetime.strptime(departure_datetime_string, '%Y-%m-%d %H:%M:%S')
         
-        economy = Seat.objects.get(type = '이코노미')
-        business = Seat.objects.get(type = '비즈니스')
-        print(economy)
-        print(business)
+        # arrival_datetime        = arrival_datetime.split(" ")
+        # arrival_datetime_string = arrival_datetime[0]+' '+arrival_datetime[1]
+        # arrival_datetime        = datetime.strptime(arrival_datetime_string, '%Y-%m-%d %H:%M:%S')
+        "2022-03-31"
 
-        # economy_seat = FlightSchedule.objects.filter
+        sort_set = {
+            "departure_time" : "departure_datetime",
+            "arrival_time"   : "arrival_datetime",
+            "duration"       : "duration",
+            "price"          : "default_price"
+        }
         
-        total_reservated_economy_seat = PassengerInformation.objects.filter(seat_id = 1).count()
-        print(total_reservated_economy_seat)
-        total_reservated_business_seat = PassengerInformation.objects.filter(seat_id = 2).count()
-        print(total_reservated_business_seat)
-        # 같은 스케쥴에 seat_id가 1이면 economy이니깐  
-        # economy_seat_remain = economy_seat_availability - total_reservated_economy_seat
-
         q = Q()
 
+        if departure_planet_id and arrival_planet_id:
+            q &= Q(departure_planet_id=departure_planet_id)
+            q &= Q(arrival_planet_id=arrival_planet_id)
+        
         if departure_datetime:
             q &= Q(departure_datetime = departure_datetime)
 
-        if arrival_datetime:
-            q &= Q(arrival_datetime = arrival_datetime)
-
-        flight_filter = FlightSchedule.objects.filter(q)
+        flight_schedules = FlightSchedule.objects.filter(q).order_by(sort_set.get(sort, "departure_time"))
 
         result = [{
             'flight_schedule_info' : {
-                'flight_schedule_id'    : flight.id,
-                'departure_planet_code' : flight.departure_planet.code,
-                'arrival_planet_code'   : flight.arrival_planet.code,
-                'departure_datetime' : flight.departure_datetime,
-                'arrival_datetime' : flight.arrival_datetime,
-                'duration' : flight.duration
+                'flight_schedule_id'    : flight_schedule.id,
+                'departure_planet_code' : flight_schedule.departure_planet.code,
+                'arrival_planet_code'   : flight_schedule.arrival_planet.code,
+                'departure_datetime'    : flight_schedule.departure_datetime,
+                'arrival_datetime'      : flight_schedule.arrival_datetime,
+                'duration'              : flight_schedule.duration
             },
-            'flight_seat_info' : {
-                # 'economy_seat_remain' 
-                # 'business_seat_remain'
-                'economy_seat_price' : economy_seat_price,
-                'business_seat_price' : business_seat_price
-            }
-        }for flight in flight_filter]
+            'flight_seat_info' : [{
+                'seat_id'     : flightseat.seat.id,
+                'seat_type'   : flightseat.seat.type,
+                'seat_remain' : flightseat.seat_availability - \
+                                flight_schedule.passengerinformation_set.filter(seat=flightseat.seat).count(),
+                'seat_price': float(flight_schedule.fligth.default_price * flightseat.seat.price_ratio/100)
+            }for flightseat in flight_schedule.fligth.flightseat_set.all()]
+        }for flight_schedule in flight_schedules]
 
         return JsonResponse({'result' : result}, status = 200)
